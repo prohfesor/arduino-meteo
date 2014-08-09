e49e6c353374e47bb5f5f54bf342dd0af708f914
prohfesor <prohfesor@gmail.com>
2014-08-09 17:20:54 +0300

    lib from adafruit

--- Visual Micro/.Meteo.vsarduino.h
+++ Visual Micro/.Meteo.vsarduino.h
@@ -5,12 +5,13 @@
 	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Arduino Mega 2560 or Mega ADK, Platform=avr, Package=arduino
+	Hardware: Arduino Nano w/ ATmega328, Platform=avr, Package=arduino
 */
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define __AVR_ATmega2560__
+#define __AVR_ATmega328p__
+#define __AVR_ATmega328P__
 #define ARDUINO 105
 #define ARDUINO_MAIN
 #define __AVR__
@@ -42,6 +43,8 @@ extern "C" void __cxa_pure_virtual() {;}
 //
 
 #include "e:\Arduino\hardware\arduino\cores\arduino\arduino.h"
-#include "e:\Arduino\hardware\arduino\variants\mega\pins_arduino.h" 
+#include "e:\Arduino\hardware\arduino\variants\eightanaloginputs\pins_arduino.h" 
 #include "C:\Users\Prof\Documents\Arduino\Meteo\Meteo.ino"
+#include "C:\Users\Prof\Documents\Arduino\Meteo\DHT.cpp"
+#include "C:\Users\Prof\Documents\Arduino\Meteo\DHT.h"
 #endif
